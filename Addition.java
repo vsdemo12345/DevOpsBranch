27001c12097975fcd5a8a6acadaf4a9819af0ea8
vsdemo <vsdemo12345@gmail.com>
2024-10-17 17:35:34 +0530

    second commit

---
+++ Addition.java
@@ -0,0 +1,13 @@
+public class Addition {
+
+public static void main(String[] main){
+
+int a = 10;
+int b = 20;
+int c = 30;
+
+int sum = a + b + c;
+System.out.println("The sum of three numbers is "+sum);
+
+}
+}
