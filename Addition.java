3823d98d1fe0292f09d732bf75f8b23a31f597cb
vsdemo12345 <vsdemo12345@gmail.com>
2024-10-17 12:10:33 +0530

    Update Addition.java

--- Addition.java
+++ Addition.java
@@ -5,9 +5,9 @@ public static void main(String[] main){
 int a = 10;
 int b = 20;
 int c = 30;
-
+//sum of three number
 int sum = a + b + c;
 System.out.println("The sum of three numbers is "+sum);
 
-}
+  }
 }
